387cca68398c087ca335cf9c85bb674ea157cb4d
Maoe <mmahechad@hotmail.com>
2019-09-15 15:38:22 -0500

    Paises

---
+++ src/app/api/country.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap, map } from 'rxjs/operators';
+import { $ } from 'protractor';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountryService {
+  private paisUrl = 'https://restcountries.eu/rest/v2/region/Americas';
+
+  constructor(private Http: HttpClient) { }
+
+  obtenerPaises(): Observable<any[]> {
+    return this.Http.get<any[]>(this.paisUrl).pipe(
+      tap(data => console.log('All: ' + JSON.stringify(data))),
+      catchError(this.handleError)
+  );
+
+  }
+  private handleError(err: HttpErrorResponse) {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+    // A client-side or network error occurred. Handle it asccordingly.
+    errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+  // The backend returned an unsuccessful response code.
+  // The response body may contain clues as to what went wrong,
+  errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+}
+
+    console.error(errorMessage);
+    return throwError(errorMessage);
+
+}
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CountryService } from '../api/country.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+paises: any [] = [];
+errorMessage: '';
+filtro: any [] = [];
+filteredProducts: any [] = [];
 
-  constructor() {}
+  constructor(private countryService: CountryService) {}
 
+  ngOnInit() {
+this.countryService.obtenerPaises().subscribe(
+  paises => {
+    this.paises = paises;
+  }, error => this.errorMessage = error
+);
+console.log(this.paises);
+  }
 }
